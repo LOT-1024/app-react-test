952bb3e9f1d792176c1a6d8e93fc1e6d09ceb88a
LOT-1024 <tunggultanvan@gmail.com>
2024-05-06 01:25:45 +0700

    update

--- src/view/components/navbar/NavbarMain.tsx
+++ src/view/components/navbar/NavbarMain.tsx
@@ -4,6 +4,7 @@ const NavbarMain = () => {
   return (
     <div className="fixed top-0 bg-white w-screen h-[120px] shadow-xl flex justify-between items-center">
         <Link to={"/"} className="text-[96px] pl-[50px]">B</Link>
+        <Link to={"/main"} className="text-[48px]">Dev</Link>
         <div className="pr-[57px] flex gap-[20px]">
             <Link to={"/books"} className="text-[48px]">Books</Link>
             <button className="text-[48px]">Logout</button>
